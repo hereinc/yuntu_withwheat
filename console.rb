8d7dd106407d454a8dc013530bdc9381713ed644
david <david@creatingev.com>
2016-01-08 13:09:57 -0800

    update sync

--- console.rb
+++ console.rb
@@ -2,4 +2,6 @@ require 'pry'
 require File.expand_path('../setup', __FILE__)
 require File.expand_path('../importer', __FILE__)
 require File.expand_path('../cohort', __FILE__)
+require File.expand_path('../ym_metric', __FILE__)
+require File.expand_path('../average_order_size', __FILE__)
 binding.pry
--- sync.rb
+++ sync.rb
@@ -3,6 +3,7 @@ require File.expand_path('../ym_metric', __FILE__)
 require File.expand_path('../cohort', __FILE__)
 require File.expand_path('../average_order_size', __FILE__)
 
+NewUsers.compute(Time.now - 15.days, Time.zone.now)
 YMMetric.sync
 NewUsers.sync
 Cohort.sync
