96bc12be2346d791a3d5cd4c57e9c8f756ac36c1
david <david@creatingev.com>
2016-01-08 16:01:18 -0800

    fix bug

--- sync.rb
+++ sync.rb
@@ -5,7 +5,7 @@ require File.expand_path('../ym_metric', __FILE__)
 require File.expand_path('../average_order_size', __FILE__)
 
 now = Time.zone.now
-Importer.new.import_all (now - 7.days).strftiem('%Y-%m-%d'), now.strftiem('%Y-%m-%d')
+Importer.new.import_all (now - 7.days).strftime('%Y-%m-%d'), now.strftime('%Y-%m-%d')
 NewUsers.compute(now - 7.days, now)
 YMMetric.sync
 NewUsers.sync
