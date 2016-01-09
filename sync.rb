4f5be3c61f590687dfe21e20ba6be1640bb3589d
david <david@creatingev.com>
2016-01-08 16:00:16 -0800

    fix bug

--- sync.rb
+++ sync.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../setup', __FILE__)
-require File.expand_path('../ym_metric', __FILE__)
+require File.expand_path('../importer', __FILE__)
 require File.expand_path('../cohort', __FILE__)
+require File.expand_path('../ym_metric', __FILE__)
 require File.expand_path('../average_order_size', __FILE__)
 
 now = Time.zone.now
